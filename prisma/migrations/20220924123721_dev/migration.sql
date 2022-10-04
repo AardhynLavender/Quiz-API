5579ab5155f959f384f3ef681bcf861602dd8ce6
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-10-04 14:23:19 +1300

    chore(migrations): clear out old migrations

--- prisma/migrations/20220924123721_dev/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Answer_Question_id_key";
-
--- DropIndex
-DROP INDEX "Answer_text_key";
--- prisma/migrations/20220928001057_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Quiz" ADD COLUMN     "question_count" INTEGER NOT NULL DEFAULT 10;
