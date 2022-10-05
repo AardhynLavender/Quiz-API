cfbe684a177ddc1749aab4a8e688e35eccdcf73c
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-10-06 11:22:51 +1300

    refactor(qa): simplification

--- quality_assurance/setup.test.ts
+++ quality_assurance/setup.test.ts
@@ -2,28 +2,24 @@ import Prisma from "../util/prismaConfig";
 import { before, after } from "mocha";
 import Actor from "./actor";
 import { AdminUser, SuperUser } from "./data/user";
-import { UserSeed } from "quality_assurance";
 import { SeedUser } from "./util/user";
 
 export const ClearDatabase = async () => {
   await Prisma.user.deleteMany();
   await Prisma.quiz.deleteMany();
+  await Prisma.question.deleteMany();
+};
+
+const SeedDatabase = async () => {
+  await SeedUser(AdminUser);
+  await SeedUser(SuperUser);
 };
 
 before((done) => {
   console.log("Pre-test: Clearing Database");
   ClearDatabase()
     .then(() => {
-      SeedUser(AdminUser as UserSeed)
-        .then(() => {
-          SeedUser(SuperUser as UserSeed)
-            .then(() => {
-              console.log("\tSuccess!");
-              done();
-            })
-            .catch(done);
-        })
-        .catch(done);
+      SeedDatabase().then(done);
     })
     .catch(done);
 });
